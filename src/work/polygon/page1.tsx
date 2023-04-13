a22870bbf3e7979ae31fa1bdff5db50adef23fd6
sahil3vedi <sahiltrivediw@gmail.com>
2023-04-14 00:11:11 +0530

    the next chapter in Ethereum scaling

--- src/work/polygon/page1.tsx
+++ src/work/polygon/page1.tsx
@@ -1,6 +1,29 @@
+import styles from './polygon.module.scss'
+
 export default function PolygonPage1(){
     return (
-        <main style={{backgroundColor:"#6D28D9"}}>
+        <main className={styles.main}>
+            <div style={{
+                backgroundColor:"#A78BFA",
+                width:"82%",
+                paddingTop:"82%",
+                position:"relative",
+                display:"block",
+                borderBottomRightRadius:"200%",
+                marginBottom:"-82%"
+            }}></div>
+            <img src={"https://seeklogo.com/images/P/polygon-matic-logo-1DFDA3A3A8-seeklogo.com.png"} style={{
+                width:"27%",
+                paddingTop:"27%",
+                position:"relative",
+                display:"block",
+                marginBottom:"-27%",
+                marginLeft:"56%"
+            }}/>
+            <div style={{marginTop:"20rem",marginLeft:"9.5rem"}}>
+                <p className={styles.titleTop}>The next chapter in</p>
+                <p className={styles.title}>Ethereum<br/>Scaling</p>
+            </div>
         </main>
     )
 }
