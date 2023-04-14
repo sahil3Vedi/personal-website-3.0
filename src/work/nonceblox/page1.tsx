001ca18ddac95b94ac8da39059b345db4026fdc4
sahil3vedi <sahiltrivediw@gmail.com>
2023-04-14 21:44:29 +0530

    web3 infra & bb

--- src/work/nonceblox/page1.tsx
+++ src/work/nonceblox/page1.tsx
@@ -1,6 +1,14 @@
+import styles from './nonceblox.module.scss'
+
 export default function NoncebloxPage1(){
     return (
-        <main style={{backgroundColor:"#BE123C"}}>
+        <main className={styles.main}>
+            <div className={styles.mainLeft}><p>Web3<br/>Infrastructure<br/>and<br/>Blockchain<br/>Bridges</p></div>
+            <div className={styles.mainRight}>
+                <div style={{backgroundColor:"#047857"}}></div>
+                <div style={{backgroundColor:"#E5E7EB"}}></div>
+                <div style={{backgroundColor:"#3F3F46"}}></div>
+            </div>
         </main>
     )
 }
