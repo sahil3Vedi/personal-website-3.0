07d26340028c870c3e58547467cf371fddba85e5
sahil3vedi <sahiltrivediw@gmail.com>
2023-04-15 20:30:29 +0530

    100% secure labels

--- src/work/checko/page1.tsx
+++ src/work/checko/page1.tsx
@@ -1,6 +1,15 @@
+import styles from './checko.module.scss'
+
 export default function CheckoPage1(){
     return (
-        <main style={{backgroundImage:"url(https://i.pinimg.com/564x/63/3b/15/633b15b8f4a2e026b209616df2ced966.jpg)"}}>
+        <main style={{
+            backgroundImage:"url(https://i.pinimg.com/564x/63/3b/15/633b15b8f4a2e026b209616df2ced966.jpg)",
+            backgroundSize:"100% 100%"
+            }}>
+            <div className={styles.mainTitle}>
+                <p className={styles.mainHeading}>100%</p>
+                <p className={styles.mainSubheading}>Secure<br/>Labels</p>
+            </div>
         </main>
     )
 }
